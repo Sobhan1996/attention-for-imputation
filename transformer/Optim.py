bc3a48f6babe26de0902d526fba63da9fec3cf27
Sobhan1996 <mniknami@uwaterloo.ca>
2020-03-30 02:34:39 -0400

    Deleted additional spaces

--- transformer/Optim.py
+++ transformer/Optim.py
@@ -13,7 +13,6 @@ class ScheduledOptim():
         self.n_warmup_steps = n_warmup_steps
         self.n_steps = 0
 
-
     def step_and_update_lr(self):
         "Step with the inner optimizer"
         lr = self._update_learning_rate()
@@ -22,17 +21,14 @@ class ScheduledOptim():
         # xm.mark_step()
         return lr
 
-
     def zero_grad(self):
         "Zero out the gradients with the inner optimizer"
         self._optimizer.zero_grad()
-
 
     def _get_lr_scale(self):
         d_model = self.d_model
         n_steps, n_warmup_steps = self.n_steps, self.n_warmup_steps
         return (d_model ** -0.5) * min(n_steps ** (-0.5), n_steps * n_warmup_steps ** (-1.5))
-
 
     def _update_learning_rate(self):
         ''' Learning rate scheduling per step '''
