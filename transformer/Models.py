8bd2cf185c36f7262bb468f7571074b2f3d3d346
Sobhan1996 <mniknami@uwaterloo.ca>
2020-03-30 23:35:58 -0400

    Added another cuda device

--- transformer/Models.py
+++ transformer/Models.py
@@ -16,7 +16,7 @@ def get_subsequent_mask(seq):
     ''' For masking out the subsequent info. '''
     sz_b, len_s = seq.size()
     subsequent_mask = (1 - torch.triu(
-        torch.ones((1, len_s, len_s), device=seq.device), diagonal=1)).bool()
+        torch.ones((1, len_s, len_s), device=torch.device("cuda:0")), diagonal=1)).bool()
     return subsequent_mask
 
 
