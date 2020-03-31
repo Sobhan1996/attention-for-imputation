fd8811bd0f1c0cd0f1fa139c57e3edfa18a74f91
Sobhan1996 <mniknami@uwaterloo.ca>
2020-03-31 16:52:30 -0400

    Fixed lr and removed division by 4

--- transformer/Optim.py
+++ transformer/Optim.py
@@ -28,14 +28,14 @@ class ScheduledOptim():
     def _get_lr_scale(self):
         d_model = self.d_model
         n_steps, n_warmup_steps = self.n_steps, self.n_warmup_steps
-        return (d_model ** -0.5) * min(n_steps ** (-0.5), n_steps * n_warmup_steps ** (-1.5) / 4)
+        return (d_model ** -0.5) * min(n_steps ** (-0.5), n_steps * n_warmup_steps ** (-1.5))
 
     def _update_learning_rate(self):
         ''' Learning rate scheduling per step '''
 
         self.n_steps += 1
         lr = self.init_lr * self._get_lr_scale()
-
+        lr = 1e-06
         for param_group in self._optimizer.param_groups:
             param_group['lr'] = lr
         return lr
