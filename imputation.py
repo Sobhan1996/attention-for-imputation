a857b2c63ba64ca4fe798c7cd69ce5896f2e9232
Sobhan1996 <mniknami@uwaterloo.ca>
2020-03-31 22:01:18 -0400

    changed parameters

--- imputation.py
+++ imputation.py
@@ -254,7 +254,7 @@ dataset = AirQualityDataset(source_dataset='./datasets/PRSA_data_2010.1.1-2014.1
                             model_file='./AirQualityData/model.chkpt', train_data=r'./AirQualityData/train.csv',
                             test_data=r'./AirQualityData/test.csv', valid_data=r'./AirQualityData/valid.csv',
                             load_data=False, load_model=False, target_column=0, target_min=0, target_max=994, d_inner=32,
-                            n_layers=1, n_head_=1, d_k=8, d_v=8, criterion=torch.nn.L1Loss(), n_warmup_steps=2000,
+                            n_layers=1, n_head_=1, d_k=32, d_v=32, criterion=torch.nn.L1Loss(), n_warmup_steps=2000,
                             target_name='pm2.5')
 dataset.train()
 # dataset.validate()
